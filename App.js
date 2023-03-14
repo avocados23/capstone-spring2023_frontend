91b838a0d44b7583f7d90a6f209cdedfe2cfe2bf
avocados23 <namht@vt.edu>
2023-03-14 07:23:41 -0400

    button

--- App.js
+++ App.js
@@ -1,34 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
-import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
 
 	const [data, setData] = useState('');
-	const [loading, setLoading] = useState(true);
+	const [loaded, setLoaded] = useState(false);
 
 	const getData = async () => {
 		try {
-			const response = await fetch('https://dummyjson.com/products/1');
+			const response = await fetch('https://rmdh-app.herokuapp.com/test');
 			const fetchedData = await response.json();
 			setData(fetchedData);
 		} catch (error) {
 			console.error(error);
 		} finally {
-			setLoading(false);
+			setLoaded(true);
 		}
 	};
 
-	useEffect(() => {
-		getData();
-	}, []);
-
   	return (
 		<View style={styles.container}>
-			{
-				loading
-				? <ActivityIndicator />
-				: <Text>{JSON.stringify(data)}</Text>
+			<Button onPress={getData} title="Press to fetch API" />
+			{ loaded
+			? <Text>{JSON.stringify(data)}</Text>
+			: <Text>Click on the button to make my API call</Text>
 			}
 		</View>
   	);
